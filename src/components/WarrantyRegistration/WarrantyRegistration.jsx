381e4dfba428adc0c2d3edeb2eb099ac2e03bf5d
gopinath18k <gopinath18k@gmail.com>
2025-07-17 10:23:44 +0530

    All Changes Are Done

--- src/components/WarrantyRegistration/WarrantyRegistration.jsx
+++ src/components/WarrantyRegistration/WarrantyRegistration.jsx
@@ -1,25 +1,7 @@
 import React from 'react'
 
 export const WarrantyRegistration = () => {
-
-  var Pumps=["Submersible","Borewell","Jet Pump","Water Pump"] 
-  
-  for(let i=0;i<Pumps.length;i++){
-    Pumps=Pumps+"/n"
-    
-  }
-    // console.log(Pumps)
   return (
-    <div>WarrantyRegistration
-      <>
-      {/* {Pumps.map((pump)=>(
-        <li key={pump.id}>{Pumps}</li>
-        
-      ))} */}
-      
-
-      
-      </>
-    </div>
+    <div>WarrantyRegistration</div>
   )
 }
