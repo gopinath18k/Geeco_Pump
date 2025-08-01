b79872a4504abf8dc98b1a959d435aff66de1237
havocguna <gunasundar64@gmail.com>
2025-08-01 17:29:36 +0530

    contact page completed , responsive too

--- src/components/Contactus/Contactus.jsx
+++ src/components/Contactus/Contactus.jsx
@@ -10,8 +10,8 @@ export const Contactus = () => {
                 <h1 className='contact-first-head'>Love To Hear From You,</h1>
                 <h1 className='contact-first-head'>Get In Touch .</h1>
                 <form action="" className='mt-5'>
-                    <div className="row">
-                        <div className="col">
+                    <div className="row contact-first-row">
+                        <div className="col-xl-6 col-lg-6 col-md-6 col-12">
                             <label htmlFor="Name" className='contact-label-field'>Your Name</label><br />
                             <input className='contact-input-box' type="text" placeholder="" style={{ width: '90%', height: '40px', fontSize: '18px' }}/>
                         </div>
@@ -28,15 +28,41 @@ export const Contactus = () => {
                         </div>
                         
                     </div>
-                </form>
-                  
-                    
-                    
-               
-                
+                </form>  
             </div>
          </div>
+{/* .................................................. Second Containtainer ............................. */}
 
+<div className="container-fluid contact-second-container">
+    <div className="contact-second-inner-container">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3915.904057534263!2d76.94486877402082!3d11.045819754168987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba85890a92ae4a3%3A0x2b6f73cfc4a56468!2sGeeco%20Pumps!5e0!3m2!1sen!2sin!4v1754027796482!5m2!1sen!2sin" className='contact-map' width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
+
+{/* ...................................................... Third Container ....................................... */}
+
+  <div className="container-fluid contact-third-container">
+    <div className="contact-third-inner-container">
+        <div className="row contact-third-row">
+            <div className="col contact-third-col"> 
+                <h3>Phone Number</h3>
+                <a href="tel:+919876543210">+91 98765 43210</a><br />
+                <a href="tel:+919876543210">+91 98765 43210</a>
+            </div>
+            <div className="col contact-third-col">
+                <h3>Mail us</h3>
+                <a href="mailto:geecopumps@gmail.com">geecopumps@gmail.com</a><br />
+                <p></p>
+            </div>
+            <div className="col contact-third-col">
+                <h3>Address</h3>
+                <p>1/42, Venkateswara Nagar,
+                   Koundampalayam, Coimbatore,
+                   Tamil Nadu 641030</p>
+            </div>
+        </div>
+    </div>
+  </div>
     </div>
   )
 }
